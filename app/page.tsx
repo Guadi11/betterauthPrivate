1e08726cf24f22e244d1a1ad15ad854aa6501f5c
NavarroRamiro14 <navarroramiro14@gmail.com>
2025-02-21 11:31:39 -0300

    un todo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <div className="w-full max-w-xs">
       <h1>BetterAuth Project</h1>
-      {/*TODO mostrar los datos del usuario ingresado*/}
+      {/*TODO mostrar los datos del usuario ingresado
+      y boton de sign out*/}
     </div>
   );
 }
