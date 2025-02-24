4bcc0d5ff3d86ec1d24485c4b141f152941a0f76
NavarroRamiro14 <navarroramiro14@gmail.com>
2025-02-24 11:51:45 -0300

    Eliminado comentario en homepage

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,6 @@ export default function Home() {
   return (
     <div className="lex flex-col items-center w-full max-w-xs">
       <h1 className="text-2xl font-bold mb-4">BetterAuth Project</h1>
-      {/*TODO mostrar los datos del usuario ingresado
-      y boton de sign out*/}
       <SessionCard/>
       <SignUpButton/>
       <SignInButton/>
