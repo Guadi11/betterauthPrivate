959071e5d1ea63c54b37b14c54228f52d1c0338a
NavarroRamiro14 <navarroramiro14@gmail.com>
2025-02-26 11:31:40 -0300

    Configuracion por si la cache se llena mucho, comentada

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /* Por si jode con la cache: 
+   webpack: (config) => {
+    config.cache = {
+      type: "filesystem",
+      compression: "gzip", // Comprime los archivos de caché
+      cacheDirectory: ".next/cache/webpack", // Ubicación de la caché
+    };
+    return config;
+  },
+  */
 };
 
 export default nextConfig;
