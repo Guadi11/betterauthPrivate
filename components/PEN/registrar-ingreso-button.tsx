623b0bf7ae81ac7fce680dd72a1456697417c29f
NavarroRamiro14 <navarroramiro14@gmail.com>
2025-03-14 10:50:52 -0300

    fixed link en boton registrar ingreso

--- components/PEN/registrar-ingreso-button.tsx
+++ components/PEN/registrar-ingreso-button.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default async function RegistrosButton(){
     return(
-        <Link href="/registrar-ingreso">
+        <Link href="/ingresos/registrar">
             <button className="mb-4 bg-yellow-400 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full">
                 Registrar Ingreso
             </button>
