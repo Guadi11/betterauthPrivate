fd4422685c84add229e79163cd72f1edefca44ea
NavarroRamiro14 <navarroramiro14@gmail.com>
2025-04-11 10:13:37 -0300

    Cambio de icono a editar diseño

--- components/Pases/editar-diseño-pat-boton.tsx
+++ components/Pases/editar-diseño-pat-boton.tsx
@@ -1,11 +1,11 @@
-import { IdCard } from "lucide-react";
+import { IdCard, PencilRuler } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
 
 export default function EditarDiseñoPatBoton(){
     return (
         <Link href="/editar_pat" className={buttonVariants({variant: "default-pat", size: "sidenav"})}>
-            <IdCard/>Editar Diseño
+            <PencilRuler/> Editar Diseño
         </Link>
     );
 }
