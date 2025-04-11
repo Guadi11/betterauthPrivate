4c9399d7b1dd6530701079003a8fb303bf90805b
NavarroRamiro14 <navarroramiro14@gmail.com>
2025-04-11 08:55:07 -0300

    Pequeño fix

--- app/(protected)/(Civil)/editar_pat/page.tsx
+++ app/(protected)/(Civil)/editar_pat/page.tsx
@@ -3,7 +3,7 @@ import EditorTarjeta from "@/components/Pases/canva";
 export default function PaginaEditarDiseño(){
     return(
         <div>
-            <h1>Pagina de confeccion de pats</h1>
+            <h1>Pagina de Edicion de Diseño de PATS</h1>
             <EditorTarjeta/>
         </div>
     );
