b8aa8beabcf6a40fc9b1b6a7d8008ce9145fef66
NavarroRamiro14 <navarroramiro14@gmail.com>
2025-05-05 11:07:11 -0300

    Protegido el boton dar ingreso

--- components/ingresos/dar-ingreso-button.tsx
+++ components/ingresos/dar-ingreso-button.tsx
@@ -1,4 +1,6 @@
 import { buttonVariants } from "@/components/ui/button";
+import { checkOrganizationAccess } from "@/lib/organization/organization-acces";
+import { ORGANIZATION_IDS } from "@/lib/organization/organization-ids";
 import { DoorOpen } from "lucide-react";
 import Link from "next/link";
 
@@ -6,7 +8,15 @@ interface Props {
   documento: string;
 }
 
-export default function DarIngresoButton({ documento }: Props){
+export default async function DarIngresoButton({ documento }: Props){
+    const organizationId = ORGANIZATION_IDS.PERSONAL_ESTABLECIMIENTOS_NAVALES;
+    
+    const accessResult = await checkOrganizationAccess({ organizationId });
+    
+    if(!accessResult.authorized){
+        return null;
+    }
+    
     return(
         <Link 
             href={`/registro/${documento}/dar_ingreso`} 
