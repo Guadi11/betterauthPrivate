358e51f3f7aec9fe148782a242e8b34b157a00c6
NavarroRamiro14 <navarroramiro14@gmail.com>
2025-09-01 12:11:04 -0300

    added h1

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,7 +3,7 @@ import SignUpForm from "@/components/sign-up-form";
 export default function SignUpPage(){
     return(
         <>
-            <h1>Registre un Usuario en el Sistema</h1>
+            <h1 className="text-2xl font-semibold tracking-tight">Crear cuenta</h1>
             <SignUpForm/>
         </>
     );
