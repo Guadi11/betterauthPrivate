8c7bca39e6989b2beb43659e447cedd9935c38df
NavarroRamiro14 <navarroramiro14@gmail.com>
2025-10-28 08:32:40 -0300

    Removida la pagina editar_pat, usaba el script de fabric para el mock up de editor de diseño.

--- components/Pases/layout-botones-pases.tsx
+++ components/Pases/layout-botones-pases.tsx
@@ -1,6 +1,5 @@
 import { checkOrganizationAccess } from "@/lib/organization/organization-acces";
 import { ORGANIZATION_IDS } from "@/lib/organization/organization-ids";
-import EditarDiseñoPatBoton from "./editar-diseño-pat-boton";
 import DiseñoPatBoton from "./diseno/disenos-boton";
 
 export default async function BotonesPasesLayout(){
@@ -15,7 +14,6 @@ export default async function BotonesPasesLayout(){
     return (
         <div className="flex-1 space-y-3">
             <DiseñoPatBoton/>
-            <EditarDiseñoPatBoton/>       
         </div>
     );
 }
